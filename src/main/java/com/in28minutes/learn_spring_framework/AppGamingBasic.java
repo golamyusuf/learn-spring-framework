f4277da49152fab3c7c8384eb95931162f4576c7
golam-yusuf <yusuf_siffo@yahoo.com>
2025-03-13 07:59:32 +0600

    Added Super Contra game class.

--- src/main/java/com/in28minutes/learn_spring_framework/AppGamingBasic.java
+++ src/main/java/com/in28minutes/learn_spring_framework/AppGamingBasic.java
@@ -2,11 +2,13 @@ package com.in28minutes.learn_spring_framework;
 
 import com.in28minutes.learn_spring_framework.game.GameRunner;
 import com.in28minutes.learn_spring_framework.game.MarioGame;
+import com.in28minutes.learn_spring_framework.game.SuperContraGame;
 
 public class AppGamingBasic {
     public static void main(String[] args) {
-        var marioGame = new MarioGame();
-        var gameRunner = new GameRunner(marioGame);
+        //var marioGame = new MarioGame();
+        var supercontraGame = new SuperContraGame();
+        var gameRunner = new GameRunner(supercontraGame);
         gameRunner.run();
     }
 }
--- src/main/java/com/in28minutes/learn_spring_framework/game/GameRunner.java
+++ src/main/java/com/in28minutes/learn_spring_framework/game/GameRunner.java
@@ -1,8 +1,8 @@
 package com.in28minutes.learn_spring_framework.game;
 
 public class GameRunner {
-    MarioGame game;
-    public GameRunner(MarioGame game) {
+    SuperContraGame game;
+    public GameRunner(SuperContraGame game) {
         this.game = game;
     }
 
